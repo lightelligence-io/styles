1c0916bdd4d18629087be432222bc532336fc649
Heinz Hölzer <heinz.hoelzer@db-n.com>
2018-10-19 08:41:02 +0000

    chore: delete unused js files
    Approved-by: Phil Schaffarzyk <phil.schaffarzyk@gridventures.de>

--- source/script/components/snackbar.js
+++
@@ -1,6 +0,0 @@
-function openSnackbar(id, timeout) {
-    document.getElementById(id).classList.add('olt-snackbar--is-open');
-    setTimeout(() => {
-        document.getElementById(id).classList.remove('olt-snackbar--is-open');
-    }, timeout);
-};
