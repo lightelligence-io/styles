52e43bd137a58c9c8fc6887c5edc96f6ba1b8787
Marco Blumendorf <marco@blumendorf.info>
2019-09-26 11:17:00 +0200

    update deployment scripts for alpha branch

--- scripts/pages-for-tag.sh
+++ scripts/pages-for-tag.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-if [[ $TRAVIS_COMMIT_MESSAGE =~ ^release ]]; then
+if [[ $TRAVIS_BRANCH == alpha ]]; then
   VERSION=$(node -p "require('./package.json').version")
   mv docs $VERSION
   mkdir docs
   mv $VERSION docs/$VERSION
 fi
+
 
