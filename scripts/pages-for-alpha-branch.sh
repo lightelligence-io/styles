32c23619248648b9d727e9059ef4bbfdab48c8c0
Marco Blumendorf <marco@blumendorf.info>
2019-09-30 13:01:06 +0200

    publish release docs

--- scripts/pages-for-alpha-branch.sh
+++ scripts/pages-for-alpha-branch.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+VERSION=$(node -p "require('./package.json').version")
+if [[ $TRAVIS_COMMIT_MESSAGE =~ ^release ]]; then
+  echo -n 'release commit: creating release docs ... '
+  mkdir -p docs/releases/$VERSION
+  ls docs/ | grep -v releases | xargs -I{} cp -r docs/{} docs/releases/$VERSION
+  echo 'done.'
+fi
+
+
 if [[ $TRAVIS_BRANCH == alpha ]]; then
-  VERSION=$(node -p "require('./package.json').version")
-  mv docs $VERSION
-  mkdir docs
-  mv $VERSION docs/$VERSION
+  echo -n 'branch alpha: creating alpha docs ... '
+  mkdir docs/$VERSION
+  ls docs/ | grep -v -e releases -e $VERSION | xargs -I{} mv docs/{} docs/$VERSION
+  echo 'done.'
 fi
 
 
