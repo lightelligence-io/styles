f5f78252cb8ac83a4bc0ad142dc01fe50d77bcfc
Marco Blumendorf <marco@blumendorf.info>
2019-10-07 15:37:54 +0200

    clone gh-pages branch and add new content

--- scripts/prepare-pages-folder.sh
+++ scripts/prepare-pages-folder.sh
@@ -17,7 +17,7 @@ if [[ $TRAVIS_BRANCH == alpha ]]; then
   echo 'done.'
 fi
 
-# checkout current gh-poages branch and add new files
+# checkout current gh-poages branch and add new docs
 echo 'creating gh-pages folder ...'
 mkdir gh-pages
 echo 'checking out gh-pages branch ...'
