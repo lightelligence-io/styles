6962cdefa1e6c45ce87ddc72641aeb059aef4864
Marco Blumendorf <marco@blumendorf.info>
2019-11-06 16:02:33 +0100

    create version.json if it does not exit

--- scripts/write-version-file.sh
+++ scripts/write-version-file.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+# version to show in the generated docs
+VERSIONFILE='docs/version.txt'
+
+# versions that can be selected in the gh-pages docs index
+VERSIONSFILE='gh-pages/versions.json'
+
+# create folders if they aren't there
 mkdir -p docs
+mkdir -p gh-pages
 
+# store current version in versions txt for current docs
 VERSION=$(node -p "require('./package.json').version")
-echo $VERSION > docs/version.txt
+echo $VERSION > $VERSIONFILE
 
-VERSIONS=$(node -p "require('./gh-pages/versions.json')")
+# add new version to versions json for gh-pages
+if [[ ! -w $VERSIONSFILE ]]; then echo [] > $VERSIONSFILE; fi
+VERSIONS=$(node -p "require('./$VERSIONSFILE')")
 NEWVERSIONS=$(node -p "JSON.stringify(($VERSIONS).concat('$VERSION'))")
-echo $NEWVERSIONS > gh-pages/versions.json
+echo $NEWVERSIONS > $VERSIONSFILE
