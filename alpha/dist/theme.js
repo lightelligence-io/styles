f424e96ef6dbebd15ab9e415c4000757b171dad8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-15 10:45:17 +0000

    Deploy lightelligence-io/styles to github.com/lightelligence-io/styles.git:gh-pages

--- alpha/dist/theme.js
+++ alpha/dist/theme.js
@@ -1,10 +1,3 @@
-exports.containerPadding = '15px';
-exports.containerMaxWidth = {
-  sm: '540px',
-  md: '720px',
-  lg: '960px',
-  xl: '1140px',
-};
 exports.breakpoint = {
   xs: 0,
   sm: '576px',
@@ -43,11 +36,7 @@ exports.gray = {
   '800': '#343a40',
   '900': '#212529',
 };
-exports.borderRadius = '2px';
-exports.borderWidth = '1px';
-exports.borderColor = 'var(--olt-gray300)';
 exports.fontFamilyBase = '\'PT Sans\', \'Arial\'';
-exports.fontFamilyMono = '\'Overpass Mono\', \'monospace\'';
 exports.fontSizeBase = '16px';
 exports.fontSize = {
   xxSmall: 'calc(var(--olt-fontSizeBase) * .65)',
@@ -77,31 +66,11 @@ exports.headingLineHeight = {
   '6': '1.75rem',
 };
 exports.lineHeightBase = '1.5';
-exports.lineHeight = {
-  xxSmall: 'calc(var(--olt-lineHeightBase) * .6667)',
-  xSmall: 'calc(var(--olt-lineHeightBase) * .8333)',
-  small: 'calc(var(--olt-lineHeightBase) * .9)',
-  medium: 'var(--olt-lineHeightBase)',
-  large: 'calc(var(--olt-lineHeightBase) * 1.3333)',
-  xLarge: 'calc(var(--olt-lineHeightBase) * 1.5333)',
-  xxLarge: 'calc(var(--olt-lineHeightBase) * 1.7333)',
-  smaller: '80%',
-  larger: '150%',
-};
 exports.fontWeight = {
   light: 400,
   regular: 400,
   bold: 700,
 };
-exports.fontWeightMono = {
-  light: 300,
-  regular: 300,
-  bold: 300,
-};
-exports.fontFamilyHeading = 'var(--olt-fontFamilyBase)';
-exports.fontWeightHeading = 'var(--olt-fontWeightBold)';
-exports.linkColor = 'var(--olt-colorPrimary)';
-exports.linkHoverContrast = '10%';
 exports.spacer = 'var(--olt-fontSizeBase)';
 exports.spacing = {
   '0': 0,
@@ -114,5 +83,3 @@ exports.spacing = {
   '7': 'calc(var(--olt-spacer) * 4.5)',
   '8': 'calc(var(--olt-spacer) * 6)',
 };
-exports.transitionDuration = '150ms';
-exports.transitionTimingFunction = 'ease';
